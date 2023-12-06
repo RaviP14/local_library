5ad30b290e7b969d14aca31990604aebe2afaec2
Ravi Patel <Ravi.Patel14@outlook.com>
2023-12-06 22:05:28 +0000

    Changed DB url and data to DB

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -25,7 +25,7 @@ app.use('/users', usersRouter);
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "localhost:27017/local_library";
+const mongoDB = "mongodb://127.0.0.1:27017/local_library";
 
 main().catch((err) => console.log(err));
 async function main() {
